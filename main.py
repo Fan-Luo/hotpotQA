e480074971df07e65b565d0bd90573c9aa228377
Fan Luo <fanluo@login2.cm.cluster>
2020-03-01 02:07:49 -0700

    add torch.backends.cudnn.enabled = False

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ def reproducibility(seed):
     torch.cuda.manual_seed(seed)
     
     #for CuDnn- a nvidia library
+    torch.backends.cudnn.enabled = False 
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
