2bcbbaa45bda75d159c5794fabe27f36cc2a1fb7
Fan Luo <fanluo@login3.cm.cluster>
2020-03-01 13:29:03 -0700

    revert to 'move set random seed from run.py to main.py, and add comet'

--- main.py
+++ main.py
@@ -99,11 +99,6 @@ def reproducibility(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.cuda.manual_seed(seed)
-    
-    #for CuDnn- a nvidia library
-    torch.backends.cudnn.enabled = False 
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
 
 reproducibility(config.seed)
 
