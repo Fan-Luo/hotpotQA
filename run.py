86a09016a782e647e8449a2887355763e02aa532
Fan Luo <fanluo@login3.cm.cluster>
2020-04-20 09:20:14 -0700

    fix

--- run.py
+++ run.py
@@ -205,7 +205,7 @@ def evaluate_batch(data_source, model, max_batches, eval_file, config):
         end_mapping = Variable(data['end_mapping'], volatile=True)
         all_mapping = Variable(data['all_mapping'], volatile=True)
 
-        logit1, logit2, predict_type, predict_support, yp1, yp2, _, _ = model(context_idxs, ques_idxs, context_char_idxs, ques_char_idxs, context_lens, start_mapping, end_mapping, all_mapping, return_yp=True)
+        logit1, logit2, predict_type, predict_support, yp1, yp2, _, _, _ = model(context_idxs, ques_idxs, context_char_idxs, ques_char_idxs, context_lens, start_mapping, end_mapping, all_mapping, return_yp=True)
         loss_1 = (nll_sum(predict_type, q_type) + nll_sum(logit1, y1) + nll_sum(logit2, y2)) / context_idxs.size(0) 
         loss_2 = nll_average(predict_support.view(-1, 2), is_support.view(-1))
         loss = loss_1 + config.sp_lambda * loss_2
