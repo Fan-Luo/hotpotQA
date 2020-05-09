165a41e86987ee3fde8f85c9699dbb9312a54114
Fan Luo <fanluo@login2.cm.cluster>
2020-05-08 20:50:05 -0700

    fix when question全是stop words

--- active_learning.py
+++ active_learning.py
@@ -417,29 +417,22 @@ def active_train(config):
     for i in range(len(train_buckets[0])):
         ques_indices = tfidf_indices[tfidf_indptr[i]:tfidf_indptr[i+1]]
         ques_words = [features[k] for k in ques_indices]  # stop words are removed
-        ques_idxs =  []
-        for w in ques_words:
-            if w in word2idx_dict:
-                ques_idxs.append(word2idx_dict[w])  
-            else:
-                ques_idxs.append(1)  # "0":"--NULL--" (padding), "1":"--OOV--" 
-                    
-        question_word_embedding_mat = word_mat[ques_idxs]
-        tfidf = tfidf_data[tfidf_indptr[i]:tfidf_indptr[i+1]]   #i-th question's tf-idf array
-        question_tfidf_weighted_embedding_mat = question_word_embedding_mat * tfidf[:, np.newaxis]
-        question_tfidf_weighted_embedding =  np.mean(question_tfidf_weighted_embedding_mat, 0) 
-        
-        if( np.isnan(question_tfidf_weighted_embedding).any() or np.isinf(question_tfidf_weighted_embedding).any() ):
-            print('question_tfidf_weighted_embedding contains NaN or infinity')    
-            print('tfidf: ', tfidf)       
-            print('ques_idxs: ', ques_idxs)
-            print('question_tfidf[i]: ', question_tfidf[i])    
-            print('ques_words: ', [features[k] for k in ques_indices])
-            print('question_list[i]: ', question_list[i])
-            print('question_tfidf_weighted_embedding_mat: ', question_tfidf_weighted_embedding_mat)
-            print('question_tfidf_weighted_embedding: ', question_tfidf_weighted_embedding)
-            input('question_tfidf_weighted_embedding contains NaN or infinity')
-            
+        if(len(ques_words) == 0):  # question全是stop words, 比如: Where can both and be found ?
+            question_tfidf_weighted_embedding = np.zeros( (300,) )
+        else:
+            ques_idxs =  []
+            for w in ques_words:
+                if w in word2idx_dict:
+                    ques_idxs.append(word2idx_dict[w])  
+                else:
+                    ques_idxs.append(1)  # "0":"--NULL--" (padding), "1":"--OOV--" 
+                        
+            question_word_embedding_mat = word_mat[ques_idxs]
+            tfidf = tfidf_data[tfidf_indptr[i]:tfidf_indptr[i+1]]   #i-th question's tf-idf array
+            question_tfidf_weighted_embedding_mat = question_word_embedding_mat * tfidf[:, np.newaxis]
+            question_tfidf_weighted_embedding =  np.mean(question_tfidf_weighted_embedding_mat, 0) 
+            assert( np.isnan(question_tfidf_weighted_embedding).any() == False and np.isinf(question_tfidf_weighted_embedding).any() == False )
+
         if i == 0:
             question_representation = question_tfidf_weighted_embedding 
         else:	  
